08512348c1fa48ed3e274e2248322279b57cd909
Matheus Prado <matheusfenoliodoprado@outlook.com>
2022-08-05 23:36:28 -0300

    Added new not coverage method.

---
+++ src/main/java/com/abinbev/productManagement/utils/Util.java
@@ -0,0 +1,8 @@
+package com.abinbev.productManagement.utils;
+
+public class Util {
+    //This code was added to test some pipeline scenarios
+    public int sumTwoIntegerNumbers(int numberOne, int numberTwo){
+        return numberOne + numberTwo;
+    }
+}
